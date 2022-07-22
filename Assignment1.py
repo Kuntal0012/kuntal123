ee28ba4f3ad8aa863e630a8cdb8dba35b8fa5c41
Kuntal0012 <105154741+Kuntal0012@users.noreply.github.com>
2022-07-23 01:20:28 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+# *
+# 'hello'
+# -87.8
+# -
+# /
+# +
+# 6
+# 
+# >values : 'hello', -87.8, 6
+# >expression: *, -, /, +
+# 
+# 
+# 2. What is the difference between string and variable?
+# >As far as python in concerned, anything you assign to a variable using quotes will be concerned will be considered as a string.
+# >Varibale is the name of the loctaion in the primary memory of the computer. This can be chosen by you. A varibale can store everything from string to numbers.
+# 
+# 
+# 3. Describe three different data types.
+# >1.String, 2.Integer, 3. List
+# 
+# 4. What is an expression made up of? What do all expressions do?
+# > An expression is made up of a sequence of operands and operators, like a + b - 10. Python supports many operators for combining data objects into expressions.
+#   Expression provides a definite result of an operator.
+#  
+# 5. This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+# >A statement is a complete line of code that performs some action, while an expression is any section of the code that evaluates to a value.
+# 
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# > 22
+# 
+# 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# 'spam' * 3
+# >'spamspamspam'
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# > Because variable name cannot start with integer.
+# 
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+# >int, float, string.
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten' + 99 + 'burritos'
+# >Because object of same types can only be concatenated.
+# >'I have eaten' + '99' + 'burritos'
+# 
+# 
